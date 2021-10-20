b72c966d9d26a9603cf27f6641f17345570b2468
Sithijadewmina <92771213+Sithijadewmina@users.noreply.github.com>
2021-10-20 13:45:43 +0530

    Update __main__.py

--- DaisyX/__main__.py
+++ DaisyX/__main__.py
@@ -63,7 +63,7 @@ from DaisyX.modules.helper_funcs.misc import paginate_modules
 from DaisyX.modules.helper_funcs.readable_time import get_readable_time
 
 PM_START_TEXT = """
-Hello there, I'm [🔥Lucy Al🔥](https://telegra.ph/file/473cc17913393959e0667.jpg)
+Hello there, I'm [🔥Lucy Al🔥](https://telegra.ph/file/8bac316b41519b7c97b71.jpg)
 
 I am an 𝐴𝑛𝑖𝑚𝑒 Themed Group Managing Bot and I will help in managing your group
 
@@ -86,7 +86,7 @@ buttons = [
 
 
 HELP_STRINGS = f"""
-*Main Commands :* [🤖](https://telegra.ph/file/473cc17913393959e0667.jpg)
+*Main Commands :* [🤖](https://telegra.ph/file/8bac316b41519b7c97b71.jpg)
 ✪ /start: Starts me! You've probably already used this.
 ✪ /help: Click this, I'll let you know about myself!
 ✪ /donate: You can support my creater using this command.
@@ -101,7 +101,7 @@ HELP_STRINGS = f"""
 
 DONATE_STRING = """Heya, glad to hear you want to donate!
 You can donate to the original writer's of the Base code,
-Support them  [Inuka](t.me/InukaASiTH),[Jason](t.me/imjanindu),"""
+Support them  [Artboylk](https://youtube.com/channel/UCFH_E0cu7U8GMjEJGnSvYjA),"""
 
 IMPORTED = {}
 MIGRATEABLE = []
